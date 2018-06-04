cc5b01259554062d9f35686e68d9c710c9ea494c
AishwaryaSivaraman <aiishwarya.sivaraman@gmail.com>
2018-06-04 16:12:55 -0700

    modified math3 version

---
+++ src/main/fperrorbound/FPSamplingVerification.java
@@ -0,0 +1,14 @@
+package fperrorbound;
+
+
+import org.apache.commons.math3.stat.inference.KolmogorovSmirnovTest;
+
+public class FPSamplingVerification {
+
+    public int generatePValue(double[] highPrecision, double[] lowPrecision){
+        int pValue = -1;
+
+        KolmogorovSmirnovTest
+        return pValue;
+    }
+}
