014884ae921ddf7045260c47cb68a32cf21579e6
AishwaryaSivaraman <aiishwarya.sivaraman@gmail.com>
2018-06-03 17:33:16 -0700

    more param in annotation

--- target/ConfidenceAnnotation.java
+++ target/ConfidenceAnnotation.java
@@ -5,5 +5,8 @@ import java.lang.annotation.Target;
 public @interface ConfidenceAnnotation {
     double epsilon = 0.03;
     double confidence = .95;
+    double precision = 0.01;
+    double min = -1000.0;
+    double max = 1000.0;
 }
 
--- target/input.java
+++ target/input.java
@@ -1,7 +1,12 @@
 
 public class Test {
-    @ConfidenceAnnotation(epsilon=0.03,
-            confidence = 95.0)
+    @ConfidenceAnnotation(
+            epsilon=0.03,
+            confidence = 95.0,
+            precision = 0.02,
+            min = 1.9,
+            max = 2.9
+    )
     public static float probCalc(float a) {
         a = a + 1000;
         for (int i = 0; i < 1000; i++) {
