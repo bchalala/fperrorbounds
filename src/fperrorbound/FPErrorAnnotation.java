06838ce3341ebf358065268771567e3c1f409e3e
John Goodlad <JohnGoodlad@outlook.com>
2018-06-04 18:43:49 -0700

    Fixed min and max

--- src/fperrorbound/FPErrorAnnotation.java
+++ src/fperrorbound/FPErrorAnnotation.java
@@ -10,14 +10,15 @@ import com.github.javaparser.ast.expr.NormalAnnotationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FPErrorAnnotation {
     public double epsilon = 0.03;
     public double confidence = .95;
     public double precision = 0.01;
-    public double[] min = {-1000.0};
-    public double[] max = {1000.0};
+    public ArrayList<Double> min = new ArrayList<>();
+    public ArrayList<Double> max = new ArrayList<>();
 
     FPErrorAnnotation(String file) throws Exception{
         HashMap<String, Integer> methodSampleMap = new HashMap<>();
@@ -29,9 +30,10 @@ public class FPErrorAnnotation {
         epsilon = methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(0).getValue().asDoubleLiteralExpr().asDouble();
         confidence = methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(1).getValue().asDoubleLiteralExpr().asDouble();
         precision = methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(2).getValue().asDoubleLiteralExpr().asDouble();
-//        min = methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(3).getValue().asDoubleLiteralExpr().asDouble()
-//        max = methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(4).getValue().asDoubleLiteralExpr().asDouble();
-
+        for(int i = 3; i < methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).size(); i+=2) {
+            min.add(methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(3).getValue().asDoubleLiteralExpr().asDouble());
+            max.add(methodAnnotationVisitor.methodAnnotations.get(methodAnnotationVisitor.method).get(4).getValue().asDoubleLiteralExpr().asDouble());
+        }
     }
 
     private static class MethodAnnotationVisitor extends VoidVisitorAdapter<Void> {
