05ee31beae3b3db2252b5eff0992fbfe4485fac2
AishwaryaSivaraman <aiishwarya.sivaraman@gmail.com>
2018-06-04 17:20:09 -0700

    annotation for multiple arguments

--- target/ConfidenceAnnotation.java
+++ target/ConfidenceAnnotation.java
@@ -6,7 +6,7 @@ public @interface ConfidenceAnnotation {
     double epsilon = 0.03;
     double confidence = .95;
     double precision = 0.01;
-    double min = -1000.0;
-    double max = 1000.0;
+    double[] min = {-1000.0};
+    double[] max = {1000.0};
 }
 
