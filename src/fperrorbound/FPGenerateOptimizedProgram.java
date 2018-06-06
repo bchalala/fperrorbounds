b8a38981ed310dca4e72b01f0980a81eb1fc95f9
AishwaryaSivaraman <aiishwarya.sivaraman@gmail.com>
2018-06-06 12:31:33 -0700

    fixed estimated time calc

--- src/fperrorbound/FPGenerateOptimizedProgram.java
+++ src/fperrorbound/FPGenerateOptimizedProgram.java
@@ -23,39 +23,42 @@ public class FPGenerateOptimizedProgram {
 
     public static long getEstimatedRunTime(FPTestProgram program, FPErrorAnnotation annotation, String methodName) throws Exception {
         Method fnFloat = FPErrorBound.returnCompiledMethod(methodName, program);
-        Method sample = FPErrorBound.returnCompiledMethod("sample",program);
+        Method sample = FPErrorBound.returnCompiledMethod(annotation.sampleMethod,program);
         long estimatedTime = 0;
         int numberOfSamples = FPSamples.generateSampleNumber(annotation.epsilon, annotation.confidence);
         int numberOfPassSamples = (int) ((annotation.confidence * numberOfSamples) / 100);
 
-        if (FPErrorBound.verifyProgram(program, annotation)) {
-            for (int i = 0; i < numberOfPassSamples; i++) {
-                double[] functionArgs = new double[annotation.min.size()];
-                for(int j=0;j<annotation.min.size();j++){
-                    //var a = sample.getParameterTypes();
-                    //Arrays.stream(a).forEach(x -> System.out.println(x.getName()));
+        for (int i = 0; i < numberOfPassSamples; i++) {
+            double[] functionArgs = new double[fnFloat.getParameterCount()];
+            for(int j=0;j<fnFloat.getParameterCount();j++){
+                //var a = sample.getParameterTypes();
+                //Arrays.stream(a).forEach(x -> System.out.println(x.getName()));
+                if(sample.getParameterCount()>0){
                     functionArgs[j] = (double) sample.invoke(null,annotation.min.get(j),annotation.max.get(j));
-                }
-                long startTime = System.nanoTime();
-
-                if(methodName.equals("fnFloat")) {
-                    if (annotation.min.size() == 1) {
-                        fnFloat.invoke(null, (float)functionArgs[0]);
-                    } else {
-                        fnFloat.invoke(null, (float)functionArgs[0], (float)functionArgs[1]);
-                    }
                 } else{
-                    if (annotation.min.size() == 1) {
-                        fnFloat.invoke(null, functionArgs[0]);
-                    } else {
-                        fnFloat.invoke(null, functionArgs[0], functionArgs[1]);
-                    }
+                    functionArgs[j] = (double) sample.invoke(null);
                 }
-                estimatedTime = estimatedTime + (System.nanoTime() - startTime);
+
             }
-
-
+            long startTime = System.nanoTime();
+            if(methodName.equals("fnFloat")) {
+                if (fnFloat.getParameterCount() < 2 ) {
+                    fnFloat.invoke(null, (float)functionArgs[0]);
+                } else {
+                    System.out.println(fnFloat.invoke(null, (float)functionArgs[0], (float)functionArgs[1]));
+                }
+            } else{
+                if (fnFloat.getParameterCount() < 2) {
+                    fnFloat.invoke(null, functionArgs[0]);
+                } else {
+                    fnFloat.invoke(null, functionArgs[0], functionArgs[1]);
+                }
+            }
+            estimatedTime = estimatedTime + (System.nanoTime() - startTime);
         }
+
+
+
         return estimatedTime;
     }
 }
