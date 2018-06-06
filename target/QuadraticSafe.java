e3c2698ddd0a16108a5d8c9518b4d76725fd3a4d
JGoodlad <JohnGoodlad@outlook.com>
2018-06-05 19:10:57 -0700

    Fixed quadratic and quadraticsafe

--- target/QuadraticSafe.java
+++ target/QuadraticSafe.java
@@ -2,14 +2,16 @@ public class Test {
     @ConfidenceAnnotation(
             epsilon=0.03,
             confidence = 95.0,
-            precision = 0.02,
-            min = 1.9,
-            max = 2.9
+            precision = 0.000001,
+            min = 195.0,
+            max = 205.0
     )
     public static float probCalc(float b) {
         float a = 1.0f;
         float c = -0.0015f;
         float x1 = (-b - (b < 0 ? -1f : 1f) * (float)Math.sqrt(b * b - 4f * a * c))/(2f * a);
-        return x1;
+        float x2 = c / (a * x1);
+        System.out.println(x1);
+        return x2;
     }
 }
