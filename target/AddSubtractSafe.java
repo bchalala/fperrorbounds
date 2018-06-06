5a05bba20eac437c62480b6da3b9e806cf433b69
JGoodlad <JohnGoodlad@outlook.com>
2018-06-05 19:32:37 -0700

    AddSubtract and AbbSubtractSafe

---
+++ target/AddSubtractSafe.java
@@ -0,0 +1,19 @@
+public class Test {
+    @ConfidenceAnnotation(
+            epsilon=0.03,
+            confidence = 95.0,
+            precision = 0.000001,
+            min = 0.5,
+            max = 0.7
+    )
+    public static float probCalc(float a) {
+        float f1 = a;
+        float f = 0.0f;
+        for(int i = 0; i < 100; i++)
+        {
+            f = f + f1;
+            f = f - f1;
+        }
+        return f;
+    }
+}
